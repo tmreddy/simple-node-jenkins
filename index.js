89b69e0766fb0735b57ba3437e4d7337223d68d4
tmreddy <t.mallikarjunreddy@gmail.com>
2025-10-02 23:54:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const PORT = 3000;
 
 app.get('/', (req, res) => res.send('Hello from Jenkins-Deployed Node App!'));
 
-app.listen(PORT, () => console.log(`App running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`App running on port ${PORT}`);
+});
